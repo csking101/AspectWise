ec3613a841cb18c852f971902a48d11542ed6db8
csking101 <42250012+csking101@users.noreply.github.com>
2023-04-23 11:08:22 +0530

    Created route validation for products

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import { useMatches, useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useMatches, useNavigate, useParams } from 'react-router-dom'
+
+const VALID_PRODUCTS = ["jbl","sony","bose"]
+
+const isValidProduct = (product) => {
+    for (let prod = 0;prod < VALID_PRODUCTS.length;prod++){
+        if (product.toLowerCase() == VALID_PRODUCTS[prod]) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 const Product = () => {
     const { product } = useParams();
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        if (!isValidProduct(product)) navigate("/")
+    })
 
   return (
     <div>
