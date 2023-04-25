db8a2857ccd01f6c301bc9898dfb68c3f57edc74
csking101 <42250012+csking101@users.noreply.github.com>
2023-04-25 07:17:34 +0530

    Added images to product list

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -16,8 +16,13 @@ const ProductsList = () => {
       <h1>List of Products</h1>
       {products.length?
       products.map((product) => {
-        return (
-          <h2>{product}</h2>
+        return (<div>
+                  <h2>{product}</h2>
+                  <img src={require("../resources/images/"+product+"1.jpg")}
+                        alt="logo"
+                  />
+                  
+                </div>
         )
       }) :<p>LOADING</p> }
     </div>
