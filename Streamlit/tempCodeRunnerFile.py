f853b5dd9ee9cb9acddbd283566063d0d29dd3d8
Adarsh Ranjan <chillingviper@gmail.com>
2024-07-06 12:48:02 +0530

    few changes

--- Streamlit/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-        # output = model(**test_sample)
-        # st.write("Logits: ",output.logits)
-        # y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
-        # st.write("Prediction: ",d[y_pred[0]])
